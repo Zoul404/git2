08ba4a112a9cff6bb3fdfc8e96985ea37453d402
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2022-08-23 10:54:33 -0300

    Create codigo.c

---
+++ codigo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int randvet()
+{
+    int i;
+    int vet[1000];
+    for(i=0;i<1000;i++)
+    {
+        vet[i]=rand();
+        printf(" %d, ",vet[i]);
+    }
+    return vet[1000];
+}
+int main()
+{
+    int vet[1000];
+    vet[1000]=randvet();
+    return 0;
+}
